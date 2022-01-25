fd7d3875dfedd8cd20fa23a0cf1ae3bdedf0ac13
AndreCosta101 <andre.costa101@gmail.com>
2022-01-24 21:44:23 -0300

    header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -14,5 +14,16 @@ export const InputSearchContainer = styled.div`
 
   input {
     width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0,0,0,0.4);
+    outline: 0;
+    padding: 0 16px;
+
+    &::placeholder {
+      color: #BCBCBC;
+    }
   }
 `;
