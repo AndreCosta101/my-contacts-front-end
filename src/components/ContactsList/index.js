cd03fcb5f223a4d1852b31e86689f5504dbe054b
AndreCosta101 <andre.costa101@gmail.com>
2022-01-26 11:12:31 -0300

    contacts list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/arrow.svg';
+import edit from '../../assets/images/edit.svg';
+import trash from '../../assets/images/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +13,73 @@ export default function ContactsList() {
         <strong>3 Contatos</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Andre Costa</strong>
+              <small>instagram</small>
+            </div>
+            <span>andre@gemail.com</span>
+            <span>(41) 9999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Andre Costa</strong>
+              <small>instagram</small>
+            </div>
+            <span>andre@gemail.com</span>
+            <span>(41) 9999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Andre Costa</strong>
+              <small>instagram</small>
+            </div>
+            <span>andre@gemail.com</span>
+            <span>(41) 9999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
