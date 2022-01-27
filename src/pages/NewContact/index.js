4ff8725c41adfe72054d691a9cb81e85bf545662
AndreCosta101 <andre.costa101@gmail.com>
2022-01-27 20:02:56 -0300

    Input and Select components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,22 @@
 import PageHeader from '../../components/PageHeader/index';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
